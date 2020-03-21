7b57f8187bbac20b3fc3e6c6e37a6771cd607d48
Marha Gumilang <marhagumilang@gmail.com>
2020-03-21 11:54:50 +0700

    initial commit

---
+++ src/main/java/com/dicoding/linebotjava/demo/LinebotjavaApplication.java
@@ -0,0 +1,13 @@
+package com.dicoding.linebotjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinebotjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinebotjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicoding/linebotjava/demo/LinebotjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicoding.linebotjava.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinebotjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
